b3e1b85bb7c95574ee670cc30b847e69158bbb54
charliemirabile <46761267+charliemirabile@users.noreply.github.com>
2022-03-22 15:44:59 -0400

    bruh moment

--- spring22/week9_mar_22/core.c
+++ spring22/week9_mar_22/core.c
@@ -1,31 +1,61 @@
 #include <linux/init.h>
 #include <linux/module.h>
+#include <linux/slab.h>
 #include <linux/platform_device.h>
+#include "shared.h"
+
+static struct platform_device *inc, *dec;
+static struct shared *shared_data;
 
 static int hello_init(void) {
 	int ret = -ENOMEM;
-	struct platform_device *child;
 	printk(KERN_INFO "Hello Module: init\n");
-	child = platform_device_alloc("example-char-device", PLATFORM_DEVID_AUTO);
-	if (!child)
+
+	shared_data = kzalloc(sizeof(*shared_data), GFP_KERNEL);
+	if (!shared_data)
 		goto fail_alloc;
 
-	ret = platform_device_add(child);
+	mutex_init(&shared_data->rw_mutex);
+
+	inc = platform_device_alloc("example-increment", PLATFORM_DEVID_AUTO);
+	if (!inc)
+		goto fail_inc;
+
+	dec = platform_device_alloc("example-decrement", PLATFORM_DEVID_AUTO);
+	if (!dec)
+		goto fail_dec;
+
+	platform_set_drvdata(inc, shared_data);
+	platform_set_drvdata(dec, shared_data);
+
+	ret = platform_device_add(dec);
 	if (ret)
-		goto fail_add;
+		goto fail_add_dec;
+
+	ret = platform_device_add(inc);
+	if (ret)
+	{
+		platform_device_unregister(dec);
+		goto fail_dec;
+	}
 
 	return 0;
 
-fail_add:
-	platform_device_put(child);
+fail_add_dec:
+	platform_device_put(dec);
+fail_dec:
+	platform_device_put(inc);
+fail_inc:
+	kfree(shared_data);
 fail_alloc:
 	return ret;
 }
 
 static void hello_exit(void) {
 	printk(KERN_INFO "Hello Module: exit\n");
-	cdev_del(hello_cdev);
-	unregister_chrdev_region(hello_device, 1);
+	platform_device_unregister(dec);
+	platform_device_unregister(inc);
+	kfree(shared_data);
 }
 module_init(hello_init);
 module_exit(hello_exit);
